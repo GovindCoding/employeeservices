a1a9edf19a902a6e7ecfd6b4921ca2439e0bc9a0
Govind Ekshinge <govindeak@gmail.com>
2024-09-18 22:19:02 +0530

    SpringSecurityConfig commit demo.

--- src/main/java/com/microservices/employeeservices/security/SpringSecurityConfig.java
+++ src/main/java/com/microservices/employeeservices/security/SpringSecurityConfig.java
@@ -1,74 +1,44 @@
-// Java Program to Illustrate Spring Security
-
 package com.microservices.employeeservices.security;
 
-// Importing required classes
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
-// Annotation
+@Configuration
 @EnableWebSecurity
-// Class
 public class SpringSecurityConfig {
-//	extends WebSecurityConfigurerAdapter {
-//
-//	// Annotation
-//	@Override
-//	protected void
-//	configure(AuthenticationManagerBuilder auth)
-//		throws Exception
-//	{
-//		auth.inMemoryAuthentication()
-//			.passwordEncoder(passwordEncoder())
-//			.withUser("gfg")
-//			.password(passwordEncoder().encode("pass"))
-//			.roles("ADMIN")
-//			.and()
-//			.passwordEncoder(passwordEncoder())
-//			.withUser("user")
-//			.password(passwordEncoder().encode("pass"))
-//			.roles("BASIC");
-//	}
-//
-//	// Annotation
-//	@Bean
-//	// Method
-//	public PasswordEncoder passwordEncoder()
-//	{
-//		return new BCryptPasswordEncoder();
-//	}
-//
-//	// Annotation
-//	@Override
-//	// Method
-//	protected void configure(HttpSecurity http)
-//		throws Exception
-//	{
-//
-//		http.authorizeRequests()
-//			.antMatchers("/basic")
-//			.hasAnyRole("BASIC", "ADMIN")
-//			.antMatchers("/admin")
-//			.hasRole("ADMIN")
-//			.antMatchers("/")
-//			.permitAll()
-//			.anyRequest()
-//			.authenticated()
-//			.and()
-//			.formLogin()
-//			.permitAll()
-//			.loginPage("/login")
-//			.usernameParameter("username")
-//			.and()
-//			.logout()
-//			.logoutRequestMatcher(
-//				new AntPathRequestMatcher("/logout"))
-//			.permitAll();
-//	}
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/", "/home").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin((form) -> form
+                        .loginPage("/login")
+                        .permitAll()
+                )
+                .logout((logout) -> logout.permitAll());
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username("user")
+                        .password("password")
+                        .roles("USER")
+                        .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
 }
